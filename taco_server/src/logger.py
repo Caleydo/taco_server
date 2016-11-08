1924c488ef0405cfeb790a50eb69e2960d9ac9e8
Samuel Gratzl <samuel_gratzl@gmx.at>
2016-11-08 10:37:15 +0100

    cleanup up files and fix paths

--- taco_server/src/logger.py
+++ taco_server/src/logger.py
@@ -1,38 +1,41 @@
+import logging
+
 __author__ = 'Reem'
 
-import logging
 
 # the logging
 
 def init_log(log_file):
-    # logging.basicConfig(level=logging.INFO,
-    #                     #format='%(message)s',
-    #                     format='%(asctime)s\t%(message)s',
-    #                     datefmt='%d-%m-%Y %H:%M:%S',
-    #                     filename=log_file,
-    #                     filemode='w')
-    logging.basicConfig(level=logging.INFO,
-                        format='%(message)s',
-                        filename=log_file,
-                        filemode='w')
-    # add a header to the log (for now)
-    message("operation", "type", "id", "position", "data") #no new id for now
+  # logging.basicConfig(level=logging.INFO,
+  #                     #format='%(message)s',
+  #                     format='%(asctime)s\t%(message)s',
+  #                     datefmt='%d-%m-%Y %H:%M:%S',
+  #                     filename=log_file,
+  #                     filemode='w')
+  logging.basicConfig(level=logging.INFO,
+                      format='%(message)s',
+                      filename=log_file,
+                      filemode='w')
+  # add a header to the log (for now)
+  message("operation", "type", "id", "position", "data")  # no new id for now
+
 
 def message(operation, type, id, position, data=None, new_id=None):
-    #todo some check that the log file already exists
-    #ignore the data for now, we don't need it
-    if data is None:
-        logging.info('%s\t%s\t%s\t%s', operation, type, id, position)
-    else:
-        logging.info('%s\t%s\t%s\t%s\t%s', operation, type, id, position, data)
-    return
+  # todo some check that the log file already exists
+  # ignore the data for now, we don't need it
+  if data is None:
+    logging.info('%s\t%s\t%s\t%s', operation, type, id, position)
+  else:
+    logging.info('%s\t%s\t%s\t%s\t%s', operation, type, id, position, data)
+  return
+
 
 def close():
-    log = logging.getLogger()
-    x = list(log.handlers)
-    for i in x:
-        log.removeHandler(i)
-        i.flush()
-        i.close()
+  log = logging.getLogger()
+  x = list(log.handlers)
+  for i in x:
+    log.removeHandler(i)
+    i.flush()
+    i.close()
 
-#todo think of a better way to store cell's IDs
+# todo think of a better way to store cell's IDs
