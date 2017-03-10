8d3ccc18907e73e262d7f589fda475ffcf5b74ea
Holger Stitz <holger.stitz@jku.at>
2017-03-10 22:53:18 +0100

    Create olympics csv by year and medal type

--- data/olympics/_dataset/count_by_year.py
+++ data/olympics/_dataset/count_by_year.py
@@ -1,57 +1,64 @@
 import csv
 
-def writeCSV(year, fieldnames, medalsPerCountry):
+def writeCSV(year, medalType, fieldnames, medalsPerCountry):
   if year is None:
     print('Invalid year -> file not written')
     return
 
+  filename = year + '_' + medalType + '.csv'
+
   print('----------------')
-  print('Write ' + lastGames + '.csv')
+  print('Write ' + filename)
   print(fieldnames)
   print(medalsPerCountry)
 
-  with open('../' + year + '.csv', 'wb') as output:
+  with open('../' + filename, 'wb') as output:
     writer = csv.DictWriter(output, fieldnames=fieldnames, restval='0', dialect='excel-tab')
     writer.writeheader()
     writer.writerows(medalsPerCountry.values())
 
   print('----------------')
 
+def readCSV(medalType = 'Total'):
+  with open('./MedalData1.csv', 'rb') as csvfile:
+    reader = csv.DictReader(csvfile, fieldnames=['Games','Sport','Event','Athlete(s)','CountryCode','CountryName','Medal','ResultInSeconds'], dialect='excel-tab')
+    next(reader)
 
-with open('./MedalData1.csv', 'rb') as csvfile:
-  reader = csv.DictReader(csvfile, fieldnames=['Games','Sport','Event','Athlete(s)','CountryCode','CountryName','Medal','ResultInSeconds'], dialect='excel-tab')
-  next(reader)
+    lastGames = None
+    fieldnames = ['CountryCode']
+    medalsPerCountry = dict()
 
-  lastGames = None
-  fieldnames = ['CountryCode']
-  medalsPerCountry = dict()
+    for row in reader:
+      if row['Games'] != lastGames:
+        # write old year when a new year is detected
+        writeCSV(lastGames, medalType, fieldnames, medalsPerCountry)
 
-  for row in reader:
-    if row['Games'] != lastGames:
-      # write old year when a new year is detected
-      writeCSV(lastGames, fieldnames, medalsPerCountry)
+        # clean up variables
+        fieldnames = ['CountryCode']
+        medalsPerCountry = dict()
 
-      # clean up variables
-      fieldnames = ['CountryCode']
-      medalsPerCountry = dict()
+      lastGames = row['Games']
+      country = row['CountryCode'] # short-cut
 
-    lastGames = row['Games']
-    country = row['CountryCode'] # short-cut
+      if row['Event'] not in fieldnames:
+        fieldnames.append(row['Event'])
 
-    if row['Event'] not in fieldnames:
-      fieldnames.append(row['Event'])
+      if row['Medal'] == medalType or medalType is 'Total':
+        if country not in medalsPerCountry:
+          medalsPerCountry[country] = dict()
+          medalsPerCountry[country]['CountryCode'] = country
 
-    if country not in medalsPerCountry:
-      medalsPerCountry[country] = dict()
-      medalsPerCountry[country]['CountryCode'] = country
+        if row['Event'] not in medalsPerCountry[country]:
+          medalsPerCountry[country][row['Event']] = 0
 
-    if row['Event'] not in medalsPerCountry[country]:
-      medalsPerCountry[country][row['Event']] = 0
+        medalsPerCountry[country][row['Event']] += 1
 
-    medalsPerCountry[country][row['Event']] += 1
+      #print(row['Games'], row['Event'], country, row['Medal'])
 
-    #print(row['Games'], row['Event'], country, row['Medal'])
+    # write the last file
+    writeCSV(lastGames, medalType, fieldnames, medalsPerCountry)
 
-  # write the last file
-  writeCSV(lastGames, fieldnames, medalsPerCountry)
-
+readCSV('Total')
+readCSV('Bronze')
+readCSV('Silver')
+readCSV('Gold')
