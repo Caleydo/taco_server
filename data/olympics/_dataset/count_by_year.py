f6df19b0c7a83d721e282451dff2ac619210d592
Holger Stitz <holger.stitz@jku.at>
2017-03-10 23:59:50 +0100

    Generate index.json dynamically based on csv files

--- data/olympics/_dataset/count_by_year.py
+++ data/olympics/_dataset/count_by_year.py
@@ -1,11 +1,21 @@
 import csv
+import json
+
+createdCSVs = []
+
+def writeIndexJson():
+  with open('../index.json', 'w') as outfile:
+    json.dump(createdCSVs, outfile)
+
 
 def writeCSV(year, medalType, fieldnames, medalsPerCountry):
   if year is None:
     print('Invalid year -> file not written')
     return
 
+  name = 'Olympic Games ' + year + ' (' + medalType + ' Medals)'
   filename = 'olympics_' + year + '_' + medalType.lower() + '.csv'
+
   # sort countries by sum of all medals
   sortedBySum = sorted(medalsPerCountry.items(), key=lambda x: sum(x[1].values()), reverse=True)
 
@@ -14,12 +24,31 @@ def writeCSV(year, medalType, fieldnames, medalsPerCountry):
   print(fieldnames)
   print(sortedBySum)
 
+  # get min and max value of the whole csv for the range
+  maxValue = float('-inf')
+  #minValue = float('inf') # does not work, because we fill empty cells with 0 by default
+
   with open('../' + filename, 'wb') as output:
     writer = csv.DictWriter(output, fieldnames=fieldnames, restval='0', dialect='excel-tab')
     writer.writeheader()
     for k, v in sortedBySum:
+      values = list(v.values())
+      maxValue = max(maxValue, max(values))
+      #minValue = min(minValue, min(values))
       v['CountryCode'] = k
       writer.writerow(v)
+
+  # build stats for index.json
+  stats = dict()
+  stats['name'] = name
+  stats['path'] = filename
+  stats['type'] = 'matrix'
+  stats['size'] = [len(sortedBySum), len(fieldnames)-1] # -1 = CountryCode fieldname
+  stats['rowtype'] = 'CountryCode'
+  stats['rowtype'] = 'SportEvent'
+  stats['value'] = dict(type='real', range=[0, maxValue])
+
+  createdCSVs.append(stats)
 
   print('----------------')
 
@@ -66,3 +95,5 @@ readCSV('Total')
 readCSV('Bronze')
 readCSV('Silver')
 readCSV('Gold')
+
+writeIndexJson()
