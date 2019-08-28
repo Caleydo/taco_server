62884394290025fcd203667dd665c806631796b9
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2019-08-28 11:39:49 +0200

    fixed indentation

--- taco_server/__init__.py
+++ taco_server/__init__.py
@@ -12,9 +12,9 @@ def phovea(registry):
   """
   # generator-phovea:begin
   registry.append('namespace', 'taco', 'taco_server.api',
-                  {
-                      'namespace': '/api/taco'
-                  })
+    {
+      'namespace': '/api/taco'
+    })
   # generator-phovea:end
   pass
 
