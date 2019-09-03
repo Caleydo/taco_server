f49ba6690a750a90d7f8e57b7e75dcd2ae5e33ea
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2019-09-03 12:34:46 +0200

    created JsonEncoder
    avoid default usage of numpy types

---
+++ taco_server/src/json_encoder.py
@@ -0,0 +1,21 @@
+"""
+Serializer to avoid default usage of numpy integer/float/bytes/etc.
+"""
+import json
+import numpy
+
+
+class JsonEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, numpy.integer):
+            return int(obj)
+        elif isinstance(obj, numpy.int64):
+            return int(obj)
+        elif isinstance(obj, bytes):
+            return obj.decode('utf-8')
+        elif isinstance(obj, numpy.floating):
+            return float(obj)
+        elif isinstance(obj, numpy.ndarray):
+            return obj.tolist()
+        else:
+            return super(JsonEncoder, self).default(obj)
